98336a041bdb2c68e79212488aae2240031be721
borislipo <boris.lipo123@gmail.com>
2022-02-02 16:05:32 -0300

    added unpkg plugin to de app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-    return <h1>hello world</h1>
+
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const submitHandler = async () => {
+        try {
+            const res = await esbuild.build({
+                entryPoints: ['index.js'],
+                bundle: true,
+                write: false,
+                plugins: [unpkgPathPlugin(input)]
+            });
+
+            setCode(res.outputFiles[0].text);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const startService = async () => {
+        try {
+            await esbuild.initialize({
+                worker: true,
+                wasmURL: '/esbuild.wasm'
+            });
+        } catch (err) {
+        }
+    }
+
+    useEffect(() => {
+        startService()
+
+    }, [])
+
+    return (
+        <div>
+            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={submitHandler}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    )
 }
 
 export default App
